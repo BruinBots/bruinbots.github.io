b634a9d104a0b4ff675d5cb6ce7fad608441fd6e
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2022-10-09 14:42:00 -0400

    add source and docs folders

--- _site/cpbuild.py
+++
@@ -1,8 +0,0 @@
-#!/Users/christophermattar/.pyenv/shims/python
-
-import os
-import shutil
-
-if os.path.exists('../BruinBots christopher-jekyll _site/docs'):
-    shutil.rmtree('../BruinBots christopher-jekyll _site/docs')
-shutil.copytree('_site', '../BruinBots christopher-jekyll _site/docs')
